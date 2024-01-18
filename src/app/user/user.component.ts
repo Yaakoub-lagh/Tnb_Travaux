eed0c3a5d8efd277784532b94b4f361517bd2629
yaakoub laghdache <laghdache.yaak@gmail.com>
2024-01-18 12:39:57 +0100

    v2

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  showAlert = false;
 
+  onSubmit() {
+    // Perform form submission logic here
+
+    // Set showAlert to true to display the alert
+    this.showAlert = true;
+
+    // Hide the alert after 3000 milliseconds (3 seconds)
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 3000);
+  }
 }
